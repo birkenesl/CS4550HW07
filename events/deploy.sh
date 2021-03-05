c7089965f448711995b7d1ff1f370ce430b170bf
Events App <events@guest.guest>
2021-03-05 21:17:17 +0000

    ready to deploy

--- events/deploy.sh
+++ events/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-export SECRET_KEY_BASE=insecure
+export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
+
 export MIX_ENV=prod
 export PORT=4795
+export DATABASE_URL=ecto://events:eventstneve@localhost/events_prod
 
 echo "Building..."
 
@@ -10,27 +12,26 @@ echo "Building..."
 mix deps.get --only prod
 mix compile
 
-CFGD=$(readlink -f ~/.config/events)
+#CFGD=$(readlink -f ~/.config/events)
 
 
-if [! -d "$CFGD" ]; then
-	mkdir -p "$CFGD"
-fi
+#if [! -d "$CFGD" ]; then
+#	mkdir -p "$CFGD"
+#fi
 
-if [! -e "$CFGD/base" ]; then
-	mix phx.gen.secret > "$CFGD/base"
-fi
+#if [! -e "$CFGD/base" ]; then
+#	mix phx.gen.secret > "$CFGD/base"
+#fi
 
-if [! -e "$CFGD/db_pass" ]; then
-	eventstneve > "$CFGD/db_pass"
-fi
+#if [! -e "$CFGD/db_pass" ]; then
+#	eventstneve > "$CFGD/db_pass"
+#fi
 
-SECRET_KEY_BASE=$(cat "$CFGD/base")
 export SECRET_KEY_BASE
 
-DB_PASS=$(cat "$CFGD/db_pass")
-export DATABASE_URL=ecto://events:$DB_PASS@localhost/events_prod
 
+export DATABASE_URL=ecto://events:eventstneve@localhost/events_prod
+#mix ecto.create
 mix ecto.migrate
 
 npm install --prefix ./assets
--- events/start.sh
+++ events/start.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-
+export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 
 export MIX_ENV=prod
 export PORT=4795
 
-CFGD=$(readlink -f ~/.config/events)
+#CFGD=$(readlink -f ~/.config/events)
 
-if [ ! -e "$CFGD/base" ]; then
-	echo "run deploy first"
-	exit 1
-fi
+#if [ ! -e "$CFGD/base" ]; then
+#	echo "run deploy first"
+#	exit 1
+#fi
 
-DB_PASS=$(cat "$CFGD/db_pass")
-export DATABASE_URL=ecto://events:$DB_PASS@localhost/events_prod
+#DB_PASS=$(cat "$CFGD/db_pass")
+export DATABASE_URL=ecto://events:eventstneve@localhost/events_prod
 
 
-SECRET_KEY_BASE=$(cat "$CFGD/base")
-export SECRET_KEY_BASE
+#SECRET_KEY_BASE=$(cat "$CFGD/base")
+#export SECRET_KEY_BASE
 
 echo "Stopping old copy of app, if any..."
 
