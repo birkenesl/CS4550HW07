25cce3fc9e5b64eb598b4bdaef534f264b506e96
Linus Birkenes <birkenes.l@northeastern.edu>
2021-03-13 13:00:33 -0500

    version for hw8 submit

--- events/lib/events_web/helpers.ex
+++ events/lib/events_web/helpers.ex
@@ -1,5 +1,6 @@
 defmodule EventsWeb.Helpers do
   alias Events.Users.User
+  alias Events.Posts.Post
   #most of this code attributed to Nat Tuck lecture 13 code
 
   def current_user_id(conn) do
@@ -9,6 +10,21 @@ defmodule EventsWeb.Helpers do
 
   def have_current_user?(conn) do
     conn.assigns[:current_user] != nil
+  end
+
+  def find_response(conn, %Post{} = post) do
+    user = conn.assigns[:current_user]
+    responses = post.responses
+    resp = Enum.find(responses, fn x -> x.user.id == user.id end)
+    if resp do
+      resp.id
+    else
+      false
+    end
+
+
+
+
   end
 
   def current_user_is_owner?(conn, %User{} = user) do
